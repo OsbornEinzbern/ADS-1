59b58cd51d6eb14fb5d6345299fc5be0abd8c698
Vio5 <160281229+OsbornEinzbern@users.noreply.github.com>
2024-03-04 22:38:38 +0300

    Upd 1.3.3 d151.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,71 +4,71 @@
 
 
 bool checkPrime(uint64_t value) {
-    for (int i = 2; i < value; i++) {
-        if (value % i == 0) {
-            return false;
-        }
+  for (int i = 2; i < value; i++) {
+    if (value % i == 0) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    uint64_t i = 2;
-    uint64_t m_cnt = 0;
-    uint64_t m_cons = 0;
-    while (m_cnt != n) {
-        uint64_t k1 = 0;
-        for (uint64_t j = 2; j < i; j++) {
-            if (i % j == 0) {
-                k1++;
-                break;
-            }
-        }
-        if (k1 == 0) {
-            m_cnt++;
-            m_cons = i;
-        }
-        i++;
+  uint64_t i = 2;
+  uint64_t m_cnt = 0;
+  uint64_t m_cons = 0;
+  while (m_cnt != n) {
+    uint64_t k1 = 0;
+    for (uint64_t j = 2; j < i; j++) {
+      if (i % j == 0) {
+        k1++;
+        break;
+      }
     }
-    return m_cons;
+    if (k1 == 0) {
+      m_cnt++;
+      m_cons = i;
+    }
+    i++;
+  }
+  return m_cons;
 }
 
 uint64_t nextPrime(uint64_t value) {
-    uint64_t m_cnt = 0;
-    uint64_t m_res = 0;
-    uint64_t i = value  + 1;
-    while (m_cnt != 1) {
-        uint64_t k1 = 0;
-        for (uint64_t j = 2; j < i; j++) {
-            if (i % j == 0) {
-                k1++;
-                break;
-            }
-        }
-        if (k1 == 0) {
-            m_cnt++;
-            m_res = i;
-        }
-        i++;
+  uint64_t m_cnt = 0;
+  uint64_t m_res = 0;
+  uint64_t i = value + 1;
+  while (m_cnt != 1) {
+    uint64_t k1 = 0;
+    for (uint64_t j = 2; j < i; j++) {
+      if (i % j == 0) {
+        k1++;
+        break;
+      }
     }
-    return m_res;
+    if (k1 == 0) {
+      m_cnt++;
+      m_res = i;
+    }
+    i++;
+  }
+  return m_res;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t sns = 0;
-    uint64_t i = 2;
-    while (i < hbound) {
-        uint64_t k1 = 0;
-        for (uint64_t j = 2; j < i; j++) {
-            if (i % j == 0) {
-                k1++;
-                break;
-            }
-        }
-        if (k1 == 0) {
-            sns += i;
-        }
-        i++;
+  uint64_t sns = 0;
+  uint64_t i = 2;
+  while (i < hbound) {
+    uint64_t k1 = 0;
+    for (uint64_t j = 2; j < i; j++) {
+      if (i % j == 0) {
+        k1++;
+        break;
+      }
     }
-    return sns;
+    if (k1 == 0) {
+      sns += i;
+    }
+    i++;
+  }
+  return sns;
 }
